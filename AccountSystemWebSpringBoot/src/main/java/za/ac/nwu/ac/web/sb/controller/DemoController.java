9396b16d58ef09bff43ef4ffe254c8ff23903b86
Ellurphurnt <70167201+Ellurphurnt@users.noreply.github.com>
2021-10-08 15:01:48 +0200

    Commit Post Controller

--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/DemoController.java
@@ -1,14 +1,15 @@
 package za.ac.nwu.ac.web.sb.controller;
 
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import za.ac.nwu.ac.domain.service.GeneralResponse;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+//import za.ac.nwu.ac.domain.s
+//
 //@RestController
 public class DemoController {
+
 //    @GetMapping("/ping")
-//    public GeneralResponse<String> ping(@RequestParam(value = "echo", defaultvalue = "pong") String echo){
-//        return new GeneralResponse<String>(successful: true, echo);
+//    public GeneralResponse<String> ping(@RequestParam(value = "echo", defaultValue = "pong") String echo) {
+//        return new GeneralResponse<String>(true, echo);
 //    }
 }
